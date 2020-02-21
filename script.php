7286794c0cbf170d375f6c2eda558d4e09c6f0b0
ELMAALM <tcsbilal@gmail.com>
2020-02-21 12:36:42 +0100

    droitprojet

---
+++ script.php
@@ -0,0 +1,40 @@
+<?php
+class conection{
+    private int a;
+    private String st;
+public conection(int a,String st){
+    this.a=a:
+    this.st=st;
+}
+public void affiche(){
+    echo"helloo word";
+    printf("le nombre: %d et le nom: %s",this.a,this.st);
+}
+    
+    
+}
+
+class con extends conection{
+    private int a;
+    private String st;
+public conection(int a,String st){
+    this.a=a:
+    this.st=st;
+}
+public void affiche(){
+    echo"helloo word";
+    printf("le nombre: %d et le nom: %s",this.a,this.st);
+}
+    
+    
+}
+
+conection a=new conection(10,"hello");
+a.affiche();
+
+echo"fghjklmù*";
+
+
+
+
+?>
